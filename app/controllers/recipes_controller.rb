bd9c6339d1c0af415b8bc7b486a9b22c5ea6c573
Carlos Sechi <carlossechi@hotmail.com>
2018-01-03 17:56:05 -0200

    user register your recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,5 +5,22 @@ class RecipesController < ApplicationController
 		@recipe = Recipe.find(id)
 	end
 
+	def new 
+		@recipe = Recipe.new
+	end
+
+	def create
+		@recipe = Recipe.new recipe_params
+		if @recipe.save
+			redirect_to @recipe
+		end
+	end
+
+	private
+
+	def recipe_params
+		params.require(:recipe).permit(:title, :recipe_type, :cuisine, :difficulty, :cook_time, :ingredients, :method)
+	end
+
 end
 
