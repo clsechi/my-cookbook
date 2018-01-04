51678c3804e4d145ab8af2e256052eb1f3e433f6
Carlos Sechi <carlossechi@hotmail.com>
2018-01-04 10:35:58 -0200

    user uses registered cuisines

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,19 +7,22 @@ class RecipesController < ApplicationController
 
 	def new 
 		@recipe = Recipe.new
+		@cuisines = Cuisine.all
 	end
 
 	def create
 		@recipe = Recipe.new recipe_params
 		if @recipe.save
 			redirect_to @recipe
+		else
+			puts "erro ao salvar os dados"
 		end
 	end
 
 	private
 
 	def recipe_params
-		params.require(:recipe).permit(:title, :recipe_type, :cuisine, :difficulty, :cook_time, :ingredients, :method)
+		params.require(:recipe).permit(:title, :recipe_type, :cuisine_id, :difficulty, :cook_time, :ingredients, :method)
 	end
 
 end
