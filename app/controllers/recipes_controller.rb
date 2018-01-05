90636969e64cfaae248d766c13c94a41affaedf9
Carlos Sechi <carlossechi@hotmail.com>
2018-01-05 15:15:22 -0200

    user edit a recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -26,6 +26,27 @@ class RecipesController < ApplicationController
 		end		
 	end
 
+	def edit
+		@recipe = Recipe.find(params[:id])
+		@cuisines = Cuisine.all
+		@recipe_types = RecipeType.all
+		render :edit
+	end
+
+	def update
+		#nao esta recebendo o objeto
+		@recipe = Recipe.find(params[:id])
+		if @recipe.update recipe_params
+			redirect_to @recipe
+		elsif @recipe.valid?
+			puts "erro ao salvar os dados"
+		else			
+			@cuisines = Cuisine.all
+			@recipe_types = RecipeType.all
+			render :new
+		end
+	end
+
 	private
 
 	def recipe_params
